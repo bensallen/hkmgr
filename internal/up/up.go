1610de9ac92e053087e37909a4bc8197bd506f9f
Benjamin S. Allen <bsallen@alcf.anl.gov>
2019-07-16 21:32:48 -0500

    Add basic execution of hyperkit based on the config.VM Cli() output

--- internal/up/up.go
+++ internal/up/up.go
@@ -2,20 +2,30 @@ package up
 
 import (
 	"fmt"
+	"os/exec"
+	"strings"
 
 	"github.com/bensallen/hkmgr/internal/config"
-	"github.com/kr/pretty"
 )
 
-func Run(cfg *config.Config) error {
+var hyperkitPath = "hyperkit"
 
+func Run(cfg *config.Config, debug bool) error {
 	for _, vm := range cfg.VM {
+		fmt.Printf("Booting VM: %s\n", vm.UUID)
+		cmdArgs := vm.Cli()
 
-		//fmt.Printf("%# v\n", pretty.Formatter(vm))
-		fmt.Printf("%# v\n", pretty.Formatter(vm.Cli()))
+		if debug {
+			fmt.Printf("cmd: %s %s\n", hyperkitPath, strings.Join(cmdArgs, " "))
+		}
 
+		cmd := exec.Command(hyperkitPath, cmdArgs...)
+		err := cmd.Start()
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			continue
+		}
+		fmt.Printf("pid: %d\n", cmd.Process.Pid)
 	}
-
 	return nil
-
 }
